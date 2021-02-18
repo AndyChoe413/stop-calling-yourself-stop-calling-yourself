e5c791aedeac9efcfa08c1d314ceab27edf4aa13
Andy Choe <Andrew.s.choe@gmail.com>
2021-02-18 15:51:37 -0500

    stop calling complete

---
+++ main.js
@@ -0,0 +1,45 @@
+const guess = str => `${str}???`
+
+const guess1 = guess('hello');
+guess1
+const guess2 = guess('what even')
+guess2    
+// 
+const twoStr = (str1, str2) => `${str1} ${str2}`
+
+const strResult1 = twoStr('oh', 'hi')
+strResult1
+const strResult2 = twoStr('well', 'hello back')
+strResult2
+// 
+const subtract = (num1, num2, num3) => num1 - num2 - num3
+
+const subtract1 = subtract(10, 1, 2)
+subtract1
+const subtract2 = subtract(8, 3, 5)
+subtract2
+const subtract3 = subtract(100, 80, 25)
+subtract3
+// 
+const reverse = (num1, num2, num3) => num3 - num2 - num1
+
+const reverse1 = reverse(10, 1, 2)
+reverse1
+const reverse2 = reverse(2, 1, 10)
+reverse2
+// 
+const triple = str => str + str + str
+
+const triple1 = triple('codeimmersives')
+triple1
+const triple2 = triple('is the best')
+triple2
+//
+const root = num => Math.sqrt(num)
+
+const root1 = root(16)
+root1
+const root2 = root(4)
+root2
+const root3 = root(9)
+root3
